fa3719dea665d9f5a0c0a1d1b2d81f4afb000372
Stephen Cartwright <ranger@Stephens-MacBook-Pro-2.local>
2017-01-18 17:55:38 -0500

    Initial Commit

---
+++ Prime Time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prime Time
+//
+//  Created by Stephen Cartwright on 1/18/17.
+//  Copyright © 2017 Ōmagatoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
